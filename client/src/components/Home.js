f50c20dfc08d76946805eae7efe858de52fdfd51
ori davidi <oridavidi100@gmail.com>
2021-12-02 13:50:36 +0200

    home page
    sets a button for going from home to chat page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return <div className='homePage'>Home</div>;
+  return (
+    <div className='homePage'>
+      <p>Home</p>
+      <h2>wellcome to chat app</h2>
+      <Link to='/chat'>to chat</Link>
+    </div>
+  );
 }
 
 export default Home;
