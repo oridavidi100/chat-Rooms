220730eecf9150713c2b0fbacbabfe2d264696f5
ori davidi <oridavidi100@gmail.com>
2021-12-02 17:42:29 +0200

    forte
    connect the fronte to the back with the axios

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import { React, useRef } from 'react';
 
 function Chat() {
+  const inputChat = useRef();
   return (
     <div className='chat-page'>
-      <div className='input-message'></div>
+      <div className='input-message'>
+        <input ref={inputChat} type='text' className='input-chat'></input>
+        <button
+          type='button'
+          onClick={() => {
+            console.log(inputChat.current.value);
+            inputChat.current.value = '';
+          }}
+        >
+          <span>&#10145;&#65039;</span>
+        </button>
+      </div>
       <div className='contact-list'></div>
       <div className='chat'></div>
     </div>
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,28 +1,30 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Login() {
+  const username = useRef();
+  const password = useRef();
+  const login = () => {
+    console.log(username.current.value, password.current.value);
+  };
   return (
     <div id='id01' className='modal'>
       <form className='modal-content animate' action='/action_page.php' method='post'>
-        <div className='imgcontainer'>
-          {/* <span onclick="document.getElementById('id01').style.display='none'" className='close' title='Close Modal'>
-            ×
-          </span> */}
-          {/* <img src='img_avatar2.png' alt='Avatar' className='avatar' /> */}
-        </div>
+        <div className='imgcontainer'></div>
         <div className='container'>
           <label htmlFor='uname'>
             <b>Username</b>
           </label>
-          <input type='text' placeholder='Enter Username' name='uname' required />
+          <input type='text' ref={username} placeholder='Enter Username' name='uname' required />
           <label htmlFor='psw'>
             <b>Password</b>
           </label>
-          <input type='password' placeholder='Enter Password' name='psw' required />
-          <Link to='/'>
-            <button type='submmit'>Login</button>
-          </Link>
+          <input type='password' ref={password} placeholder='Enter Password' name='psw' required />
+          {/* <Link to='/'> */}
+          <button type='button' onClick={login}>
+            Login
+          </button>
+          {/* </Link> */}
         </div>
         <div className='container' style={{ backgroundColor: '#f1f1f1' }}></div>
       </form>
